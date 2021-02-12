ebf312714ba689099bc11b8b6cdbe7dc94ca2074
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-02-12 12:43:56 +0100

    header done

--- header.php
+++ header.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>The Band</title>
 </head>
 <body>
-    
-</body>
-</html>
+<header>
+<a href="index.php">
+    <img src="css/img/logo.png" id="logo" alt="logo">
+</a>
+<nav>
+    <ul class="flex-right">
+        <a href="index.php"><li>Medlemmer</li></a>
+        <a href="index.php"><li>Live</li></a>
+        <a href="index.php"><li>Merchandise</li></a>
+        <a href="index.php"><li>Info</li></a>
+    </ul>
+</nav>
+</header>
